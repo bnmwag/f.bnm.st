63bb629f2bc94327223de275907fd2f76d16da7e
bw <bw@cic.at>
2024-09-12 22:47:25 +0200

    fix: add vercel intergration

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,13 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import lenis from "astro-lenis";
 import glsl from "vite-plugin-glsl";
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
 	integrations: [tailwind(), lenis()],
+	output: "server",
+	adapter: vercel(),
 	vite: {
 		plugins: [glsl()], // top-level await present in the "four" library
 	},
