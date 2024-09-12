9623a1a37559100ce906b830106564e628509409
bw <bw@cic.at>
2024-09-12 23:13:33 +0200

    feat: sitemap.xml & robots.txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,9 +4,20 @@ import tailwind from "@astrojs/tailwind";
 import lenis from "astro-lenis";
 import glsl from "vite-plugin-glsl";
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
-	integrations: [tailwind(), lenis()],
+	site: "https://f.bnmwag.dev",
+	integrations: [
+		tailwind(),
+		lenis(),
+		sitemap({
+			changefreq: "never",
+			priority: 0.5,
+			lastmod: new Date("2024-09-12T21:11:38.271Z"),
+		}),
+	],
 	output: "static",
 	vite: {
 		plugins: [glsl()], // top-level await present in the "four" library
