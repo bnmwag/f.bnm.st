bc6baac24034054a96ffff3c2c7f681fbb223cf3
bw <bw@cic.at>
2024-09-12 23:05:25 +0200

    fix: 404 on assets error

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,13 +3,11 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import lenis from "astro-lenis";
 import glsl from "vite-plugin-glsl";
-import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
 	integrations: [tailwind(), lenis()],
-	output: "server",
-	adapter: vercel(),
+	output: "static",
 	vite: {
 		plugins: [glsl()], // top-level await present in the "four" library
 	},
