bf0cdf082f87c93e1c7a3768cac98e867720d9a0
bw <bw@cic.at>
2024-09-14 02:35:54 +0200

    refactor: code archirecture

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,13 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				background: 'var(--background)',
+				foreground: 'var(--foreground)',
+				'foreground-muted': 'var(--foreground-muted)',
+			},
+		},
 	},
 	plugins: [],
 }
