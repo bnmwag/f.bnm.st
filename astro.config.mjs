479454919b25f82fbeff7567c7f6ef141ccd57e9
bnm <bnm.wag@gmail.com>
2024-10-14 22:45:59 +0200

    feat: dockerize app

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,25 +1,28 @@
-// @ts-check
-import { defineConfig } from "astro/config";
-import tailwind from "@astrojs/tailwind";
-import lenis from "astro-lenis";
-import glsl from "vite-plugin-glsl";
+import { defineConfig } from 'astro/config'
+import tailwind from '@astrojs/tailwind'
+import lenis from 'astro-lenis'
+import glsl from 'vite-plugin-glsl'
+import node from '@astrojs/node'
 
-import sitemap from "@astrojs/sitemap";
+import sitemap from '@astrojs/sitemap'
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://f.bnmwag.dev",
+	site: 'https://f.bnmwag.dev',
 	integrations: [
 		tailwind(),
 		lenis(),
 		sitemap({
-			changefreq: "never",
+			changefreq: 'never',
 			priority: 0.5,
-			lastmod: new Date("2024-09-12T21:11:38.271Z"),
+			lastmod: new Date('2024-09-12T21:11:38.271Z'),
 		}),
 	],
-	output: "static",
+	output: 'server',
+	adapter: node({
+		mode: 'standalone',
+	}),
 	vite: {
 		plugins: [glsl()], // top-level await present in the "four" library
 	},
-});
+})
